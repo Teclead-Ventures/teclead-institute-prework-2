f5eb7bd083464eebbaf9393d84c26d8955fedb97
Dom van Almsick <dominicvana@gmail.com>
2022-07-21 13:22:28 +0100

    feat: add instructions to warmup exercises

--- src/warmup/warmup.js
+++ src/warmup/warmup.js
@@ -1,27 +1,99 @@
 /*
 WARMUP
+
+If you are stuck or need a hint, check out the test file warmup.test.js.
+
+To test your solutions, run the following command in your terminal:
+
+npm t warmup
 */
 
+/*
+Complete the function reverseString below.
+
+It has one parameter, a string.
+It should return a string containing the characters of the input string in reverse order.
+*/
 function reverseString(str) {}
 
+/*
+Complete the function sum below.
+
+It has one parameter (an array of numbers).
+It should return the sum of all the numbers in the array.
+*/
 function sum(arr) {}
 
+/*
+Complete the function mean below.
+
+It has one parameter (an array of numbers).
+It should return the arithmetic mean of all the numbers in the array.
+*/
 function mean(nums) {}
 
+/*
+Complete the function findMax below.
+
+It has one parameter (an array of numbers).
+It should return the greatest number in the array.
+*/
 function findMax(arr) {}
 
+/*
+Complete the function uppercase below.
+
+It has one parameter (an array of strings).
+It should return an array of strings with all the strings in the input array uppercased.
+*/
 function uppercase(arr) {}
 
+/*
+Complete the function capitalize below.
+
+It has one parameter (an array of strings).
+It should return an array of strings with the first letter of each string capitalized, and all other letters lowercased.
+*/
 function capitalize(arr) {}
 
+/*
+Complete the function snakeCase below.
+
+It has one parameter (a string).
+It should return a string containing the input string with all the spaces replaced with underscores, and the first letter of each word lowercased.
+*/
 function snakeCase(str) {}
 
+/*
+Complete the function incrementBy below.
+
+It has two parameters (an array of numbers, and a number).
+It should return an array of numbers with each number in the input array incremented by the second parameter.
+*/
 function incrementBy(nums, incrementVal) {}
 
+/*
+Complete the function appendTo below.
+
+It has two parameters (an array of strings, and a string).
+It should return an array of strings with the second parameter appended to the end of each string in the input array.
+*/
 function appendTo(stringsArr, appendVal) {}
 
+/*
+Complete the function insertAt below.
+
+It has three parameters (an array of strings, a string, and a number).
+It should return an array of strings with the second parameter inserted at the index of the third parameter.
+*/
 function insertAt(stringsArr, insertVal, index) {}
 
+/*
+Complete the function wordCounts below.
+
+It has one parameter (a string).
+It should return an object containing the number of times each word appears in the input string.
+*/
 function wordCounts(str) {}
 
 module.exports = {
