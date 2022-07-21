0726e8f01a820d5f0be2551e3afe077d3ca2e4fc
Dom van Almsick <dominicvana@gmail.com>
2022-07-21 13:37:06 +0100

    feat: add instructions to types exercises

--- src/types/types.js
+++ src/types/types.js
@@ -2,22 +2,76 @@
 THINKING ABOUT TYPES
 */
 
+/*
+Complete the function isNumber below.
+
+It has one parameter (a value).
+It should return true if the value is a number, and false otherwise.
+*/
 function isNumber(value) {}
 
+/*
+Complete the function isArray below.
+
+It has one parameter (a value).
+It should return true if the value is an array, and false otherwise.
+*/
 function isArray(value) {}
 
+/*
+Complete the function isObject below.
+
+It has one parameter (a value).
+It should return true if the value is an object, and false otherwise.
+*/
 function isObject(value) {}
 
+/*
+Complete the function isFunction below.
+
+It has one parameter (a value).
+It should return true if the value is a function, and false otherwise.
+*/
 function isFunction(value) {}
 
+/*
+Complete the function isNaN below.
+
+It has one parameter (a value).
+It should return true if the value is NaN, and false otherwise.
+*/
 function isNaN(value) {}
 
+/*
+Complete the function toNumbers below.
+
+It has one parameter (an array of values).
+It should return an array of numbers, where each number is the result of coercing the corresponding value in the input array to be a number.
+*/
 function toNumbers(array) {}
 
+/*
+Complete the function toStrings below.
+
+It has one parameter (an array of values).
+It should return an array of strings, where each string is the result of coercing the corresponding value in the input array to be a string.
+*/
 function toStrings(arr) {}
 
-function removeNums(arr) {}
+/*
+Complete the function removeNumbers below.
 
+It has one parameter (an array of values).
+It should return an the same array of values, exccept that each number is removed.
+*/
+function removeNumbers(arr) {}
+
+/*
+Complete the function removeStrings below.
+
+It has one parameter (an array of values).
+It should return an the same array of values, exccept that each string is removed.
+*/
 function removeStrings(arr) {}
 
 module.exports = {
