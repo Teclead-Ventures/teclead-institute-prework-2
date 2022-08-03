3c84f0d60e1deb007036fe53384b3f02f4deb825
Lukas Zöllner <lukas.zoellner@teclead-ventures.de>
2022-08-03 10:42:45 +0200

    add remaining tests for types challenge

--- src/types/types.test.js
+++ src/types/types.test.js
@@ -131,3 +131,51 @@ describe('toNumbers()', () => {
     expect(toNumbers([1, 2, 3, [4, 5], [6]])).toEqual([1, 2, 3, NaN, 6]);
   });
 });
+
+describe('toStrings()', () => {
+  test('should return an array of strings, where each element is the result of coercing the corresponding value in the input array to be a string.', () => {
+    expect(toStrings([])).toEqual([]);
+    expect(toStrings(['1', '2', '3'])).toEqual(['1', '2', '3']);
+    expect(toStrings([1, 2, 3, '4', '5'])).toEqual(['1', '2', '3', '4', '5']);
+    expect(toStrings([undefined])).toEqual(['undefined']);
+    expect(toStrings([null])).toEqual(['null']);
+    expect(toStrings([{}])).toEqual(['[object Object]']);
+    expect(toStrings([() => 0])).toEqual(['() => 0']);
+    expect(toStrings([NaN])).toEqual(['NaN']);
+    expect(toStrings([[]])).toEqual(['']);
+    expect(toStrings([0, [1, 2, 3, 'a', () => 0, null, undefined]])).toEqual([
+      '0',
+      '1,2,3,a,() => 0,,',
+    ]);
+  });
+});
+
+describe('removeNumbers()', () => {
+  test('should return an array containing all elements that are NOT numbers, while maintaining the order of those elements', () => {
+    const func = () => 0;
+    expect(removeNumbers([])).toEqual([]);
+    expect(removeNumbers(['1', '2', '3'])).toEqual(['1', '2', '3']);
+    expect(removeNumbers([1, 2.442, 3, '4', '5', 1])).toEqual(['4', '5']);
+    expect(removeNumbers([undefined])).toEqual([undefined]);
+    expect(removeNumbers([null])).toEqual([null]);
+    expect(removeNumbers([{}])).toEqual([{}]);
+    expect(removeNumbers([func])).toEqual([func]);
+    expect(removeNumbers([NaN])).toEqual([]);
+    expect(removeNumbers([[]])).toEqual([[]]);
+  });
+});
+
+describe('removeStrings()', () => {
+  test('should return an array containing all elements that are NOT strings, while maintaining the order of those elements', () => {
+    const func = () => 0;
+    expect(removeStrings([])).toEqual([]);
+    expect(removeStrings(['1', '2', '3'])).toEqual([]);
+    expect(removeStrings([1, 2, 3, '4', '5', 1])).toEqual([1, 2, 3, 1]);
+    expect(removeStrings([undefined])).toEqual([undefined]);
+    expect(removeStrings([null])).toEqual([null]);
+    expect(removeStrings([{}])).toEqual([{}]);
+    expect(removeStrings([func])).toEqual([func]);
+    expect(removeStrings([NaN])).toEqual([NaN]);
+    expect(removeStrings([[]])).toEqual([[]]);
+  });
+});
